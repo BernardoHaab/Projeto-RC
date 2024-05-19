21d8bc5f310f71dfefe112bc906e2810734d38bd
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-05-19 17:35:40 +0100

    refactor: ignore unused parameter on class/post-received-hook

--- src/command/class/post-received-hook.c
+++ src/command/class/post-received-hook.c
@@ -1,38 +1,41 @@
 #include "tcp/socket.h"
 
-#include <assert.h>
+static void classDefaultPostReceiveHook(const TCPSocket socket)
+{
+	(void) socket;
+}
 
 void classHelpPostReceiveHook(const TCPSocket socket)
 {
-	(void) socket;
+	classDefaultPostReceiveHook(socket);
 }
 
 void classLoginPostReceiveHook(const TCPSocket socket)
 {
-	(void) socket;
+	classDefaultPostReceiveHook(socket);
 }
 
 void classListClassesPostReceiveHook(const TCPSocket socket)
 {
-	(void) socket;
+	classDefaultPostReceiveHook(socket);
 }
 
 void classListSubscribedPostReceiveHook(const TCPSocket socket)
 {
-	(void) socket;
+	classDefaultPostReceiveHook(socket);
 }
 
 void classSubscribePostReceiveHook(const TCPSocket socket)
 {
-	(void) socket;
+	classDefaultPostReceiveHook(socket);
 }
 
 void classCreateClassPostReceiveHook(const TCPSocket socket)
 {
-	(void) socket;
+	classDefaultPostReceiveHook(socket);
 }
 
 void classSendPostReceiveHook(const TCPSocket socket)
 {
-	(void) socket;
+	classDefaultPostReceiveHook(socket);
 }
